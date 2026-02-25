51e431a0dedb8a75dc5605be7e570f6b394f178e
jit singha <jitsingha570@gmail.com>
2026-02-25 11:03:37 +0530

    QuipBill project setup

---
+++ src/main/java/com/QuipBill_server/QuipBill/QuipBillApplication.java
@@ -0,0 +1,13 @@
+package com.QuipBill_server.QuipBill;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuipBillApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuipBillApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/QuipBill_server/QuipBill/QuipBillApplicationTests.java
@@ -0,0 +1,13 @@
+package com.QuipBill_server.QuipBill;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuipBillApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
