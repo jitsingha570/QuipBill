6d10f719520286283b70d6afb31b0f5a0977e208
jit singha <jitsingha570@gmail.com>
2026-03-27 00:18:05 +0530

    cors connection staging domain

--- src/main/java/com/QuipBill_server/QuipBill/config/CorsConfig.java
+++ src/main/java/com/QuipBill_server/QuipBill/config/CorsConfig.java
@@ -16,7 +16,8 @@ public class CorsConfig {
                 registry.addMapping("/**")
                         .allowedOrigins(
                                 "http://localhost:5173",
-                                "https://quipbill.vercel.app"
+                                "https://quipbill.vercel.app",
+                                "https://quipbill-staging.vercel.app"
                         )
                         .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                         .allowedHeaders("*")
